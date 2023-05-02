529a8c424ba2c2de4af5819c823c08826a685a80
heukwu <yoo4068@gmail.com>
2023-05-02 15:53:30 +0900

    숫자의 합

---
+++ 숫자의_합/Main.java
@@ -0,0 +1,20 @@
+package 숫자의_합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split("");
+        int sum = 0;
+
+        for (String s1 : s) {
+            sum += Integer.parseInt(s1);
+        }
+
+        System.out.println(sum);
+    }
+}
