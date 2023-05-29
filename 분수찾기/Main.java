c5d9bc32e5e8d3ee98d47b30894adc3e8a4153be
heukwu <yoo4068@gmail.com>
2023-05-29 14:19:21 +0900

    분수찾기

--- 분수찾기/Main.java
+++ 분수찾기/Main.java
@@ -8,6 +8,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int x = Integer.parseInt(br.readLine());
+        int count = 0;
+        int i = 1;
 
+        while (count < x) {
+            count += i;
+            i++;
+        }
+
+        int b = 0;
+        int a = 0;
+
+        if ((i - 1) % 2 == 0) {
+            b = 1 + (count - x);
+            a = (i - 1) - (count - x);
+        } else {
+            b = (i - 1) - (count - x);
+            a = 1 + (count - x);
+        }
+
+        System.out.println(a + "/" + b);
     }
 }
