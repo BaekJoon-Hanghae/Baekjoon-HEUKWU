7ed3dd2edaf7a2fd63c4148a5d7efb65f3bdd50b
heukwu <yoo4068@gmail.com>
2023-07-09 19:52:03 +0900

    바구니 뒤집기**

--- 바구니_뒤집기/Main.java
+++ 바구니_뒤집기/Main.java
@@ -9,29 +9,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n + 1];
 
-        for (int i = 0; i <= n; i++) {
+        int[] arr = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
             arr[i] = i;
         }
 
+        StringTokenizer st2;
         for (int i = 0; i < m; i++) {
-            StringTokenizer s = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(s.nextToken());
-            int b = Integer.parseInt(s.nextToken());
+            st2 = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st2.nextToken());
+            int b = Integer.parseInt(st2.nextToken());
 
             while (a < b) {
-                int c = arr[a];
+                int temp = arr[a];
                 arr[a++] = arr[b];
-                arr[b--] = c;
+                arr[b--] = temp;
             }
         }
 
         for (int i = 1; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-
     }
 }
