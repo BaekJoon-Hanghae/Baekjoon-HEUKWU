7d1d6ffa1a3570b99f8b60f9bfb0550aa37251bd
heukwu <yoo4068@gmail.com>
2023-07-10 22:03:39 +0900

    체스판 다시칠하기**

--- 체스판_다시_칠하기/Main.java
+++ 체스판_다시_칠하기/Main.java
@@ -9,44 +9,40 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int row = Integer.parseInt(st.nextToken());
         int col = Integer.parseInt(st.nextToken());
 
         String[] board = new String[row];
+
         for (int i = 0; i < row; i++) {
             board[i] = br.readLine();
         }
 
-        // 1. 체스판 자르기
-        int sol = Integer.MAX_VALUE;
+        int count = Integer.MAX_VALUE;
         for (int i = 0; i < row - 7; i++) {
             for (int j = 0; j < col - 7; j++) {
-                // 2. 체스판의 최소 비용 구하기
-                int curSol = getSolution(i, j, board);
-                // 3. 전체 최적의 값과 비교하여 갱신하기
-                if (sol > curSol) {
-                    sol = curSol;
-                }
+                count = Math.min(count, method(i, j, board));
             }
         }
 
-        System.out.println(sol);
+        System.out.println(count);
     }
 
-    private static int getSolution(int startRow, int startCol, String[] board) {
-        String[] orgBoard = {"WBWBWBWB", "BWBWBWBW"};
-        int whiteSol = 0;
-        for (int i = 0; i < 8; i++) {
-            int row = startRow + i;
-            for (int j = 0; j < 8; j++) {
-                int col = startCol + j;
-                if (board[row].charAt(col) != orgBoard[row % 2].charAt(j)) {
-                    whiteSol++;
+    private static int method(int i, int j, String[] board) {
+        String[] answer = {"WBWBWBWB", "BWBWBWBW"};
+        int count = 0;
+
+        for (int k = 0; k < 8; k++) {
+            int a = i + k;
+            for (int l = 0; l < 8; l++) {
+                int b = j + l;
+
+                if (board[a].charAt(b) != answer[a % 2].charAt(l)) {
+                    count++;
                 }
             }
-
         }
-        return Math.min(whiteSol, 64 - whiteSol);
+
+        return Math.min(count, 64 - count);
     }
 }
