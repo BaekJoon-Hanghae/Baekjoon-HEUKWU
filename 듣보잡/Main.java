16575bbe777e6054bf777bc22bfd6a5a667e0b3a
heukwu <yoo4068@gmail.com>
2023-07-19 10:44:45 +0900

    듣보잡

---
+++ 듣보잡/Main.java
@@ -0,0 +1,41 @@
+package 듣보잡;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Set<String> l = new TreeSet<>();
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            l.add(br.readLine());
+        }
+
+        for (int i = 0; i < m; i++) {
+            String s = br.readLine();
+            if (l.contains(s)) {
+                list.add(s);
+            }
+        }
+
+        Collections.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(list.size()).append('\n');
+
+        for (String s : list) {
+            sb.append(s).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
