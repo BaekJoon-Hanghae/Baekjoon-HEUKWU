731517c86445eb8f067e71f8b6cb5b4ae130d022
heukwu <yoo4068@gmail.com>
2023-07-21 10:08:18 +0900

    이진수

---
+++ 이진수/Main.java
@@ -0,0 +1,25 @@
+package 이진수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t; i++) {
+            String s = Integer.toBinaryString(Integer.parseInt(br.readLine()));
+            for (int j = s.length() - 1; j >= 0; j--) {
+                if (s.charAt(j) == '1') {
+                    sb.append(s.length() - 1 - j).append(" ");
+                }
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
