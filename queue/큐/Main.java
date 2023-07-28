ed18ae9d2ce45f101d32380965dd67009430129a
heukwu <yoo4068@gmail.com>
2023-07-28 09:11:11 +0900

    큐

---
+++ queue/큐/Main.java
@@ -0,0 +1,56 @@
+package queue.큐;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] split = br.readLine().split(" ");
+            String s = split[0];
+
+            switch (s) {
+                case "pop":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.poll());
+                    }
+                    break;
+                case "size":
+                    System.out.println(queue.size());
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.peek());
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.getLast());
+                    }
+                    break;
+                case "push":
+                    queue.add(Integer.parseInt(split[1]));
+            }
+        }
+    }
+}
