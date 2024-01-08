5397b29b98356fe426b140394454f4daf125167c
heukwu <yoo4068@gmail.com>
2024-01-08 09:50:23 +0900

    binary search-02 (부품 찾기)

---
+++ ThisIsCodingTest/first/binary_search/p197_부품_찾기/Main.java
@@ -0,0 +1,52 @@
+package ThisIsCodingTest.first.binary_search.p197_부품_찾기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+
+        int m = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            if (binarySearch(a, Integer.parseInt(st.nextToken()), 0, n - 1) != -1) {
+                sb.append("yes");
+            } else {
+                sb.append("no");
+            }
+            sb.append(" ");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
